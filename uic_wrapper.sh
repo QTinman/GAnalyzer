01fa6b5a879fa154f00c636465bf37e8d82b7c87
QTinman <jonssofh@hotmail.com>
2023-10-19 19:04:48 +0000

    Remove unnecessary files

--- uic_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-LD_LIBRARY_PATH=/home/john/Qt/5.13.1/Src/qtbase/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH
-QT_PLUGIN_PATH=/home/john/Qt/5.13.1/Src/qtbase/plugins${QT_PLUGIN_PATH:+:$QT_PLUGIN_PATH}
-export QT_PLUGIN_PATH
-exec /home/john/Qt/5.13.1/Src/qtbase/bin/uic "$@"
